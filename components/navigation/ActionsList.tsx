eaf6e2f0cfa47a7967fe410d12778abddbf6fcb8
naarkz <naarkz@michie.dev>
2023-06-17 03:15:37 +0800

    feat: Highlight active router in actions list

--- components/navigation/ActionsList.tsx
+++ components/navigation/ActionsList.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function ActionsList({
@@ -8,6 +9,7 @@ export default function ActionsList({
   variant: "large" | "small";
   wrapperStyles?: string;
 }) {
+  const { asPath } = useRouter();
   return (
     <ul
       className={`border-2 font-bold text-primary-red max-w-[370px] ${
@@ -18,10 +20,10 @@ export default function ActionsList({
         variant === "large" ? "gap-y-[18px]" : "gap-y-[12px]"
       } ${variant === "large" ? "text-center" : "text-start"} ${wrapperStyles}`}
     >
-      <li className="hover:underline">
+      <li className={`hover:underline ${asPath.includes("/subreddits") ? "text-primary-dark" : ""}`}>
         <Link href={"/subreddits"}>Join or Download (your) Subreddits</Link>
       </li>
-      <li className="hover:underline">
+      <li className={`hover:underline ${asPath.includes("/saved-items") ? "text-primary-dark" : ""}`}>
         <Link href={"/saved-items"}>
           Mass Save or Download (your) saved-items
         </Link>

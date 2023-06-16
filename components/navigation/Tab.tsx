d28d2915bc84c80ba5fa9e86568a8fb961b2dc01
naarkz <naarkz@michie.dev>
2023-06-17 03:19:01 +0800

    feat: Highlight active tab

--- components/navigation/Tab.tsx
+++ components/navigation/Tab.tsx
@@ -22,14 +22,18 @@ export default function Tab({
       <div className="flex items-center justify-center">
         <span
           onClick={() => toggleActiveTab("a")}
-          className="font-bold text-lg text-primary-red cursor-pointer px-[23px] py-[7px]"
+          className={`text-lg  cursor-pointer px-[23px] py-[7px] ${
+            activeTab === "a" ? "text-primary-dark font-semibold" : "text-primary-red font-bold"
+          }`}
         >
           {titleA}
         </span>
         <div className="w-[2px] h-[26px] bg-primary-red"></div>
         <span
           onClick={() => toggleActiveTab("b")}
-          className="font-bold text-lg text-primary-red cursor-pointer px-[23px] py-[7px]"
+          className={`text-lg  cursor-pointer px-[23px] py-[7px] ${
+            activeTab === "b" ? "text-primary-dark font-semibold" : "text-primary-red font-bold"
+          }`}
         >
           {titleB}
         </span>

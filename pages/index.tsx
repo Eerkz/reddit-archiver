06dc9f92544d302a2a1b863f6de320ad76904bca
naarkz <naarkz@michie.dev>
2023-06-17 14:46:30 +0800

    fix: Consumer errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import { GetServerSidePropsContext } from "next";
-import { RedditIdentity } from "../types/RedditUser";
-import { useState } from "react";
 import { handleAuth } from "../lib/auth";
 import MainLayout from "../components/layouts/MainLayout";
 import Signin from "../components/landing/Signin";
 import ActionsList from "../components/navigation/ActionsList";
 import { useCurrentUser } from "../store/userContext";
 
-export default function Home() {
+export default function HomePage() {
   const { user } = useCurrentUser();
   return (
     <MainLayout>
-      {!user ? <Signin /> : <ActionsList variant="large" />}
+      {user ? <ActionsList variant="large" /> : <Signin />}
     </MainLayout>
   );
 }
